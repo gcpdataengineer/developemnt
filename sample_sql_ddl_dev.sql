fc04b13838dcaceff19ef7b6ab0ef705d2f647c5
gcpdataengineer <126462768+gcpdataengineer@users.noreply.github.com>
2023-02-27 15:40:48 +0530

    sample ddl upoaded
    sample ddl upoaded

---
+++ sample_sql_ddl_dev.sql
@@ -0,0 +1,5 @@
+CREATE TABLE
+  cde_qt_hyd_dev.test_1( id integer NOT NULL OPTIONS ( description = 'customer_id'),
+    name string,
+    salary float64 OPTIONS ( description = ' salary of employee '),
+    doj date );
